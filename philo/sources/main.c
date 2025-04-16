7029354537602678aed8097e589753852daa72c5
Elisa Delannoy <elisadelannoy10@gmail.com>
2025-04-16 10:27:44 +0200

    Makefile

---
+++ philo/sources/main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edelanno <edelanno@student.42perpignan.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/15 15:06:18 by edelanno          #+#    #+#             */
+/*   Updated: 2025/04/15 15:43:05 by edelanno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+// void	*routine()
+// {
+// 	printf("test\n");
+// }
+
+int	main(void)
+{
+	// pthread_t	t1;
+	
+	// // pthread_create(&t1, NULL, &routine, NULL);
+	// pthread_join(t1, NULL);
+}
