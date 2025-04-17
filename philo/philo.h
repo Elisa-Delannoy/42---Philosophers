2605ce0c5e2536b745f40964a1cabb5de0d43268
Elisa Delannoy <elisadelannoy10@gmail.com>
2025-04-17 18:55:12 +0200

    init fork and routine to modify

--- philo/philo.h
+++ philo/philo.h
@@ -2,21 +2,42 @@
 # define PHILO_H
 
 # include <stdlib.h>
+# include <string.h>
 # include <stdio.h>
 # include <unistd.h>
 # include <pthread.h>
-
-int	ft_atoi(const char *nptr);
+# include <sys/time.h>
 
 typedef struct s_philo
 {
-	pthread_t	threads;
-	int			nb_philo;
+	int					id;	
+	pthread_mutex_t		left_fork;
+	pthread_mutex_t		right_fork;
+	int					last_meal;
+	pthread_t			threads;
+	size_t				t_init_action;
+	struct s_var		*var;
 }	t_philo;
 
 
-// typedef struct s_var
-// {
+typedef struct s_var
+{
+	int				nb_philo;
+	int				t_die;
+	size_t			t_eat;
+	size_t			t_sleep;
+	int				nb_eat;
+	// size_t			t_init_action;
+	pthread_mutex_t	*fork;
+	t_philo			*philo;
+}	t_var;
 
-// }	t_var var;
+// typedef struct s_timeval
+// {
+// 	time_t		tv_sec;
+// 	suseconds_t	tv_usec;
+// }	t_timeval;
+
+int	ft_atoi(const char *nptr);
+
 #endif
--- philo/sources/main.c
+++ philo/sources/main.c
@@ -6,76 +6,154 @@
 /*   By: edelanno <edelanno@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 15:06:18 by edelanno          #+#    #+#             */
-/*   Updated: 2025/04/16 15:28:48 by edelanno         ###   ########.fr       */
+/*   Updated: 2025/04/17 18:54:27 by edelanno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
 
-void	*routine()
+size_t	ft_get_time(void)
 {
-	// printf("test\n");
+	struct timeval	tv;
+	size_t		time_ms;
+	
+	gettimeofday(&tv, NULL);
+	time_ms = (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
+	return (time_ms);
+}
+
+int	check_philo_is_dead(t_var var)
+{
+	if (ft_get_time() - var.philo->last_meal > var.t_eat)
+		return (0);
+	else
+		return (1);
+}
+
+int	ft_take_fork(t_var var)
+{
+	if (var.nb_philo == 1)
+		return (1);
+	else if (var.nb_philo % 2 == 0) /*test later for impair*/
+	{
+		if (var.philo->id % 2 == 1)
+		{
+			pthread_mutex_lock(&var.philo->left_fork);
+			pthread_mutex_lock(&var.philo->right_fork);
+		}
+		else
+		{
+			pthread_mutex_lock(&var.philo->right_fork);
+			pthread_mutex_lock(&var.philo->left_fork);
+		}
+		printf("philo = %d is eating\n", var.philo->id);	
+	}
+	return (0);
+}
+
+void	*routine(void *arg)
+{
+	t_philo *philo = (t_philo*)arg;
+	
+	while(1)
+	{
+		if (check_philo_is_dead(*philo->var) == 0)
+			break;
+		if (ft_take_fork(*philo->var) == 1)
+			break;
+		if (ft_get_time() - philo->t_init_action > philo->var->t_eat)
+		{
+			pthread_mutex_unlock(&philo->left_fork);
+			pthread_mutex_unlock(&philo->right_fork);
+		}
+			
+	}
+	
+	// while(ft_get_time() - philo->t_init_action < philo->var->t_eat)
+	// 	printf("eat");
+	// printf("\n");
+	// // usleep(1000000);
+	// philo->last_meal = ft_get_time();
+	// philo->t_init_action = ft_get_time();
+	// // printf("init %zu\n", philo->var->t_init_action);
+	// // printf("sleep %zu\n",  philo->var->t_sleep);
+	// // printf("diff %zu\n",  ft_get_time() - philo->t_init_action);
+	// while(ft_get_time() - philo->t_init_action < philo->var->t_sleep)
+	// 	printf("sleep");
+	// printf("\n");
+	// // printf("think\n");
 	return (NULL);
 }
 
-void	ft_take_fork(t_philo philo, int i)
-{
-	if (philo.nb_philo % 2 == 0)
-	{
-		if (i % 2 == 0)
-			printf("philo = %d is eating\n" ,i);
-	}
-}
 
-void	ft_create_philosphers(t_philo philo)
-{
-	int			i;
-
-	i = 0;
-	while (i < philo.nb_philo)
-	{
-		if (pthread_create(philo.threads + i, NULL, &routine, NULL) != 0)
-			return;
-		ft_take_fork(philo, i + 1);
-		// printf("philo = %d is created\n" , i + 1);
-		i++;
-	}
-}
-
-void	ft_kill_philosophers(t_philo philo)
+void	ft_kill_philosophers(t_var var)
 {
 	int	i;
 	
-	i = 1;
-	while (i <= philo.nb_philo)
+	i = 0;
+	while (i < var.nb_philo)
 	{
-		if (pthread_join(philo.threads[i - 1], NULL) != 0)
+		if (pthread_join(var.philo[i].threads, NULL) != 0)
 			return;
-		// printf("philo = %d finished execution\n" ,i);
+		// printf("philo = %d finished execution\n" ,i + 1);
 		i++;
 	}
 }
 
-t_philo	ft_init(char **argv)
+void	ft_init_action_philo_and_fork(t_var *var)
 {
-	t_philo	philo;
+	size_t	i;
+
+	i = 0;
+	while (i < (size_t)var->nb_philo)
+	{
+		var->philo[i].id = i + 1;
+		var->philo[i].right_fork = i; 
+		var->philo[i].left_fork = (i + 1) % var->nb_philo;
+		var->philo[i].last_meal = 0;
+		var->philo[i].t_init_action = ft_get_time();
+		var->philo[i].var = var;
+		if (pthread_mutex_init(&var->fork[i], NULL) != 0)
+			return ;		
+		if (pthread_create(&var->philo[i].threads, NULL, &routine, &var->philo[i]) != 0)
+			return ;
+		i++;
+	}
+}
+
+t_var	ft_init_action(char **argv)
+{
+	t_var	var;
 	
-	philo.nb_philo = ft_atoi(argv[1]);
-	philo.threads = malloc(philo.nb_philo * sizeof(pthread_t));
-	return (philo);
+	var.nb_philo = ft_atoi(argv[1]);
+	var.t_die = ft_atoi(argv[2]);
+	var.t_eat = ft_atoi(argv[3]);
+	var.t_sleep = ft_atoi(argv[4]);
+	if (argv[5])
+		var.nb_eat = ft_atoi(argv[5]);
+	else
+		var.nb_eat = -1;
+	var.fork = malloc (var.nb_philo * sizeof(pthread_mutex_t));
+	var.philo = malloc(var.nb_philo * sizeof(t_philo));
+	if (!var.fork || !var.philo)
+	{
+		var.nb_philo = -1;
+		return (var);
+	}
+	ft_init_action_philo_and_fork(&var);
+	return (var);
 }
 
 int	main(int argc, char **argv)
 {
-	t_philo	philo;
+	t_var	var;
 	
-	if (argc == 4 || argc == 5)
+	if (argc == 5 || argc == 6)
 	{
-		philo = ft_init(argv);
-		if (philo.nb_philo == 1)
-			return 1;
-		ft_create_philosphers(philo);
-		ft_kill_philosophers(philo);
+		var = ft_init_action(argv);
+		// if (var.nb_philo == 1)
+		// 	return 1;
+		ft_kill_philosophers(var);
 	}
 	else
 		return 0;
